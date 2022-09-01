a1e8e692cf12b1141911d9ef1d30d5fea1092c55
mahmoudgaber97 <94110835+mahmoudgaber97@users.noreply.github.com>
2022-09-01 20:54:11 +0200

    Add files via upload
    USART for STM32

---
+++ USART/MUSART_interface.h
@@ -0,0 +1,10 @@
+#ifndef MUSART_INTERFACE_H_
+#define MUSART_INTERFACE_H_
+#include"TYPES.h"
+void MUSARTSetvoid_boudRate(f32 f32boudRate);
+void MUSARTIntivoid(void);
+void Sendu8String(u8* u8String);
+void Send_u8Char(u8 u8Char);
+u8 Receive_u8Char(void);
+u8* Receive_u8String(void);
+#endif
